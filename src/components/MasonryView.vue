03aa136b0bd9101a2ad6e0aa902bc49cda7322cf
Zyuhel <zyuhel@yandex.ru>
2023-08-01 00:51:40 +0300

    fix(): fix warnings

--- src/components/MasonryView.vue
+++ src/components/MasonryView.vue
@@ -2,7 +2,7 @@
 import PhotoCard from "@/components/PhotoCard.vue";
 
 defineProps<{
-  items: array, userpage: boolean
+  items: Array, userpage: boolean
 }>()
 </script>
 
@@ -10,7 +10,7 @@ defineProps<{
   <div class="uk-child-width-1-2@s uk-child-width-1-3@m" uk-grid="masonry: true"
        uk-lightbox="animation: slide; toggle: .lightbox-link" v-if="items.length>0">
     <div v-for="item in items" class=" uk-flex" :key="item.message_id">
-      <PhotoCard :item="item" :show_detail="userpage" />
+      <PhotoCard :item="item" :show_detail="userpage" :for_calendar="false" />
     </div>
   </div>
 </template>
